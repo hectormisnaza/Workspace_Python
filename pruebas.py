77274368d6e8281a70d3caa9be36bd7a9941115d
hectormisnaza <h.misnaza@misena.edu.co>
2022-05-12 10:22:14 -0500

    update 20220512 AM2

--- pruebas.py
+++ pruebas.py
@@ -18,3 +18,14 @@ sector_size = 512
 sector_amount = disk_size * sector_amount
 
 print(sector_amount)
+
+# REPLACE THIS STARTER CODE WITH YOUR FUNCTION
+june_days = 30
+print("June has " + str(june_days) + " days.")
+july_days = 31
+print("July has " + str(july_days) + " days.")
+
+def month_days(month, days):
+    print(month + " has " + str(days) + " days.")
+month_days("June", 30)
+month_days("July", 31)
